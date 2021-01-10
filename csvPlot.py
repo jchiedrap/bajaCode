3d180f11ce951595709c2405875b3a154a098a5c
star-fruit <52576397+star-fruit@users.noreply.github.com>
2021-01-10 18:11:05 -0500

    Replacing datetime object with time object

--- csvPlot.py
+++ csvPlot.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
-#Old code from Jan 6th with the one-liner
+from datetime import time
+#Finished Jan 6th 2020
 def plotData(path, quantity):
 
     df = pd.read_csv(path)
 
-    time = [datetime(int(timestring[3:7]), int(timestring[1:3]), int(timestring[0:1]), int(timestring[7:9]), int(timestring[9:11]), second = int(timestring[11:13])) for timestring in [str(df.loc[index, 'DDMMYYYYHHMMSS']) for index, row in df.iterrows()]]
-        
-    plt.plot(time, df[quantity])
+    timeArray = [time( int(timestring[7:9]), int(timestring[9:11]), second = int(timestring[11:13])) for timestring in [str(df.loc[index, 'DDMMYYYYHHMMSS']) for index, row in df.iterrows()]]
+   
+    timeArray = pd.to_datetime(timeArray, format="%H:%M:%S")#Converting into pandas time format (with standard year, month, day)
+    plt.xticks(rotation=30)
+    
+    plt.plot(timeArray, df[quantity])
     plt.title(f'{quantity} vs Time')
     plt.xlabel('Time')
     plt.ylabel(f'{quantity}')
     plt.show()
+    
