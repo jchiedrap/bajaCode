5ee49b8cf2241be13009195dd522d5bad5dabf20
star-fruit <52576397+star-fruit@users.noreply.github.com>
2021-01-10 18:07:51 -0500

    One-liner to the date-time code

--- csvPlot.py
+++ csvPlot.py
@@ -1,22 +1,12 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
-#Old Code from Jan 6th
+#Old code from Jan 6th with the one-liner
 def plotData(path, quantity):
 
     df = pd.read_csv(path)
-    time = []
 
-    for index, row in df.iterrows():
-        timestring = str(df.loc[index,'DDMMYYYYHHMMSS'])
-        year = int(timestring[3:7]) ###
-        month = int(timestring[1:3]) ###
-        day = int(timestring[0:1]) ###
-        hour = int(timestring[7:9])
-        minute = int(timestring[9:11])
-        second = int(timestring[11:13])
-
-        time.append(datetime(year, month, day, hour, minute, second))
+    time = [datetime(int(timestring[3:7]), int(timestring[1:3]), int(timestring[0:1]), int(timestring[7:9]), int(timestring[9:11]), second = int(timestring[11:13])) for timestring in [str(df.loc[index, 'DDMMYYYYHHMMSS']) for index, row in df.iterrows()]]
         
     plt.plot(time, df[quantity])
     plt.title(f'{quantity} vs Time')
