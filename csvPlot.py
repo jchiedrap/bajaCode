49a1756716439891a6af87f641acb0f22c8a79af
jchiedrap <61250863+jchiedrap@users.noreply.github.com>
2021-05-07 21:48:31 -0400

    Added type declaration decorators to csvPlot.py

--- csvPlot.py
+++ csvPlot.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import time
 #Finished Jan 6th 2020
-def plotData(path, quantity):
+def plotData(path: str, quantity: str):
 
     df = pd.read_csv(path)
 
     timeArray = [time( int(timestring[7:9]), int(timestring[9:11]), second = int(timestring[11:13])) for timestring in [str(df.loc[index, 'DDMMYYYYHHMMSS']) for index, row in df.iterrows()]]
    
-    timeArray = pd.to_datetime(timeArray, format="%H:%M:%S")#Converting into pandas time format (with standard year, month, day)
+    timeArray = pd.to_datetime(timeArray, format="%H:%M:%S") #Converting into pandas time format (with standard year, month, day)
     plt.xticks(rotation=30)
     
     plt.plot(timeArray, df[quantity])
