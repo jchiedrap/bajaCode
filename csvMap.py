ca424fbed7ff526a24f3ebd0f5e1077d1765e288
star-fruit <52576397+star-fruit@users.noreply.github.com>
2021-05-29 16:18:07 -0400

    Added code to create csv files into a csv folder

--- csvMap.py
+++ csvMap.py
@@ -5,6 +5,29 @@ from pandas.core.frame import DataFrame
 def turnToDf(filePath: str):
     return pd.read_csv(filePath)
 
+def splitCsvInDfIfDuplicateHeaders(filePath: str): #Make this create separate data frames instead of csv's
+    df = turnToDf(filePath)
+    dfList = []
+    #preLine is last line to iterate the csv from
+    #curLine is current line to iterate the csv to
+    #csvNumber is the current number of the csv generated, starting with 1
+    prevLine, curLine, csvNumber = 0, 0, 0
+    for index, row in df.iterrows():
+        curLine += 1  
+        if df.loc[index,"RPM"] == "RPM":
+            currDf = df.iloc[prevLine:curLine-1]#Skips over line where the repeat header was recognized
+            dfList.append(currDf)
+            prevLine = curLine
+    lastDf = df.iloc[prevLine:]
+    dfList.append(lastDf)
+    return dfList
+
+def createDirectory(folderName: str, childFolders: list): #Creates working directory within the user's home directory on Windows
+    BAJAFolder = os.path.join(os.environ['USERPROFILE'], folderName)
+    if not os.path.exists(BAJAFolder):
+        for folder in childFolders:
+            os.makedirs(os.path.join(BAJAFolder, folder))
+
 def colorPick (val: float, minVal: float, maxVal: float):
     if val >= maxVal or val <= minVal: # Bounds are exclusive
         return '#FFFFFF' # out of bounds, return white 
@@ -26,34 +49,12 @@ def colorPick (val: float, minVal: float, maxVal: float):
    
     return '#{:02x}{:02x}{:02x}'.format(red, green, blue)
 
-def createDirectory(folderName: str, childFolders: list): #Creates working directory within the user's home directory on Windows
-    BAJAFolder = os.path.join(os.environ['USERPROFILE'], folderName)
-    if not os.path.exists(BAJAFolder):
-        for folder in childFolders:
-            os.makedirs(os.path.join(BAJAFolder, folder))
-
 def createHTMLFile(mapPlot: gmplot.GoogleMapPlotter,folderName: str, outputName: str):
     mapPlot.draw(os.path.join(os.environ['USERPROFILE'], folderName, 'html', outputName))
     
 def openHTMLFile(folderName: str, outputName: str):
     os.system('"' + os.path.join(folderName, 'html', outputName) + '"')
 
-def splitCsvInDfIfDuplicateHeaders(filePath: str): #Make this create separate data frames instead of csv's
-    df = pd.read_csv(filePath)
-    dfList = []
-    #preLine is last line to iterate the csv from
-    #curLine is current line to iterate the csv to
-    #csvNumber is the current number of the csv generated, starting with 1
-    prevLine, curLine, csvNumber = 0, 0, 0
-    for index, row in df.iterrows():
-        curLine += 1  
-        if df.loc[index,"RPM"] == "RPM":
-            currDf = df.iloc[prevLine:curLine-1]#Skips over line where the repeat header was recognized
-            dfList.append(currDf)
-            prevLine = curLine
-    lastDf = df.iloc[prevLine:]
-    dfList.append(lastDf)
-    return dfList
 
 def mapDf(df: DataFrame, 
           folderName: str, 
@@ -91,12 +92,20 @@ def turnToExcel(df: DataFrame, folderName: str, outName: str, fileNum: int = 1):
         df.to_excel(os.path.join(os.environ['USERPROFILE'], folderName, 'xlsx', outName + '.xlsx'), 
                 index=False)
 
+def turnToCSV(df: DataFrame, folderName: str, outName: str, fileNum: int = 1):
+    if fileNum != 1:
+        df.to_csv(os.path.join(os.environ['USERPROFILE'], folderName, 'csv', outName + '_' + str(fileNum) + '.csv'), 
+                index=False)
+    else:
+        df.to_csv(os.path.join(os.environ['USERPROFILE'], folderName, 'csv', outName + '.csv'), 
+                index=False)
+
 def processData(path: str, 
                 outName: str, 
                 variableToBeMeasured: str, 
                 minVal: float, maxVal: float, 
                 folderName: str = 'BAJAPlots', 
-                childFolders: list = ['html', 'xlsx']
+                childFolders: list = ['html', 'xlsx', 'csv']
                ):
     dfList = splitCsvInDfIfDuplicateHeaders(path)
     createDirectory(folderName, childFolders)
@@ -106,3 +115,4 @@ def processData(path: str,
         fileNum += 1
         mapDf(dataFrame, folderName, outName, variableToBeMeasured, minVal, maxVal, fileNum)
         turnToExcel(dataFrame, folderName, outName, fileNum)
+        turnToCSV(dataFrame, folderName, outName, fileNum)
